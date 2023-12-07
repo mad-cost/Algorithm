e6464ce754d4a24536748d9abb5d62629d5c94ee
mad-cost <148847167+mad-cost@users.noreply.github.com>
2023-12-07 21:44:32 +0900

    [Silver V] Title: 숫자 카드, Time: 952 ms, Memory: 152152 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -5,42 +5,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-//    5
-//    6 3 2 10 -10
-//    8
-//    10 9 -5 2 3 4 5 -10
-//    1. Set(Collection Frameworks) & contains 이용
-//    2. 이진탐색(Bindary Search) 이용
-    public void solution() throws IOException {
+    //    Set
+//    binarySearch 이진탐색
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); //5
-        String[] myCardString = br.readLine().split(" "); //6, 3, 2, 10, -10
+        int M = Integer.parseInt(br.readLine());
+        String[] myCard = br.readLine().split(" ");
 
-//        1. Hash Set & contains
         Set<Integer> myCards = new HashSet<>();
-        for (int i = 0; i < N; i++) {
-//            myCards에 myCardString을 형변환 후 넣어주기
-//            6, 3, 2, 10, -10
-            myCards.add(Integer.parseInt(myCardString[i]));
+        for (int i = 0; i < myCard.length; i++) {
+            myCards.add(Integer.parseInt(myCard[i]));
+            // myCards = 6 5 3 2 10 -10
         }
-        int M = Integer.parseInt(br.readLine()); // 8
-//        10 9 -5 2 3 4 5 -10
-        String[] inCard = br.readLine().split(" ");
+        int N = Integer.parseInt(br.readLine());
+        String[] standard = br.readLine().split(" ");
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-//            contains = HashSet내의 객체가 존재하는지 true, false 반환
-//            ? 1 : 0; / true 일경우 1 반환 false 일경우 0반환
-            int result = myCards.contains(Integer.parseInt(inCard[i]))
-                    ? 1 : 0;
+        for (int i = 0; i < standard.length; i++) {
+            int result = myCards.contains(Integer.parseInt(standard[i])) ? 1 : 0;
             sb.append(result).append(" ");
+
         }
-        System.out.println(sb);
-
-    }
-
-    public static void main(String[] args) throws IOException{
-        Main sol = new Main();
-        sol.solution();
+        System.out.println(sb.toString());
     }
 }
