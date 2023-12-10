731e164a617a1c88e35ade191b8d5d4bfcf129b7
mad-cost <148847167+mad-cost@users.noreply.github.com>
2023-12-11 00:41:00 +0900

    [level unrated] Title: 문자열 돌리기, Time: 119.30 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            char answer = input.charAt(i);
+            System.out.println(answer);
+        }
+    }
+}
