2a66474f64ff09260bb93a4aed957b77275202a6
mad-cost <148847167+mad-cost@users.noreply.github.com>
2023-12-10 23:33:47 +0900

    [level unrated] Title: 2의 영역, Time: 1.44 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -1,32 +1,26 @@
 public class Solution {
     public int[] solution(int[] arr) {
-        int leftIndex = -1;
+        int front = -1;
+        int back = 0;
+
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == 2){
-//                왼쪽인덱스의 2를 찾으면 발견 중단.
-                leftIndex = i;
+            if (arr[i] == 2){
+                front = i;
                 break;
             }
         }
-        if(leftIndex == -1){
+        if (front == -1)
             return new int[]{-1};
-        }
-        
-        
-        int rightIndex = 0;
-        for (int i = arr.length -1; i > -1; i--) {
-            if(arr[i] ==2){
-                rightIndex = i;
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            if (arr[i] == 2){
+                back = i;
                 break;
             }
         }
-        int[] answer = new int[rightIndex - leftIndex +1];
-//        arr = 1,2,1,4,5,2,9 / answer = [5-1+1]
-        for (int i = leftIndex; i < rightIndex +1; i++) {
-            answer[i - leftIndex] = arr[i];
-        }
-        for (int i = 0; i < rightIndex -leftIndex +1; i++){
-            answer[i] = arr[i + leftIndex];
+        int[] answer = new int[back - front + 1];
+        for (int i = front; i < back + 1; i++) {
+            answer[i - front] = arr[i];
         }
         return answer;
     }
