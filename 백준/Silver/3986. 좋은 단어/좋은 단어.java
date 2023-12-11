1901cf99a0d061bf3cfb953b74ecb1b74ce4ba98
mad-cost <148847167+mad-cost@users.noreply.github.com>
2023-12-12 00:08:41 +0900

    [Silver IV] Title: 좋은 단어, Time: 328 ms, Memory: 23072 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine()); //3
+        // 좋은 단어가 몇개일까
+        int goodWords = 0; //count
+        for (int i = 0; i < N; i++) {
+            // 단어를 받는다.
+            String word = reader.readLine(); // 단어 3개 받음
+            // 단어가 짝을 잘 맞추는지를 스택으로 확인한다.
+            Stack<Character> charStack = new Stack<>(); // char형 스택 생성
+            // 각 글자를 확인하며,
+            for (char c : word.toCharArray()) {
+                // 스택이 비어있거나 제일 위의 글자가 현재 글자와 다르면 push
+                if (charStack.empty() || charStack.peek() != c) charStack.push(c);
+                    // 제일 위의 글자가 현재 글자와 같으면 pop
+                else if (charStack.peek() == c) charStack.pop();
+            }
+            // 글자를 다 확인하고 스택이 비어있으면 좋은 단어이다.
+            if (charStack.empty()) goodWords++;
+        }
+
+        // 갯수 출력
+        System.out.println(goodWords);
+    }
+}
