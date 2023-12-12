9ca906674e0f74147783bc5ac94226033b7b90ee
mad-cost <148847167+mad-cost@users.noreply.github.com>
2023-12-13 00:09:17 +0900

    [level unrated] Title: 수 조작하기 2, Time: 8.71 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -2,22 +2,21 @@ public class Solution {
     public String solution(int[] numLog) {
         StringBuilder sb = new StringBuilder();
 
-//        인덱스를 하나씩 순회 length -1
-        for (int i = 0; i < numLog.length - 1; i++) {
-            int diff = numLog[i +1] - numLog[i];
+        for (int i = 0; i < numLog.length-1; i++) {
+            int diff = numLog[i+1] - numLog[i];
+
             switch (diff){
-//                diff 가 1일때 실행.
                 case 1:
-                    sb.append('w');
+                    sb.append("w");
                     break;
                 case -1:
-                    sb.append('s');
+                    sb.append("s");
                     break;
                 case 10:
-                    sb.append('d');
+                    sb.append("d");
                     break;
                 case -10:
-                    sb.append('a');
+                    sb.append("a");
                     break;
             }
         }
