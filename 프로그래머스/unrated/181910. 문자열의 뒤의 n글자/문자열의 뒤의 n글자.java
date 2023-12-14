3fb363cd97b8b937717d689d7f284ed04bdd64bd
mad-cost <148847167+mad-cost@users.noreply.github.com>
2023-12-14 17:02:15 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,9 +1,7 @@
 public class Solution {
     public String solution(String my_string, int n) {
-        int startIndex = my_string.length() -n;
-
         StringBuilder sb = new StringBuilder();
-        for (int i = startIndex; i <my_string.length() ; i++) {
+        for (int i = my_string.length()-n; i < my_string.length(); i++) {
             sb.append(my_string.charAt(i));
         }
         return sb.toString();
