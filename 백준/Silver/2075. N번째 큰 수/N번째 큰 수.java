3b073ad51c3e943ede86e9adeec6b6831090e83c
mad-cost <148847167+mad-cost@users.noreply.github.com>
2024-03-11 16:24:49 +0900

    [Silver II] Title: N번째 큰 수, Time: 940 ms, Memory: 341500 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,27 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(reader.readLine());
-    // 최솟값이 우선적으로 나오는 우선순위 큐
-    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
 
     for (int i = 0; i < n; i++) {
-      String[] lineNums = reader.readLine().split(" ");
+      String[] lineNums = br.readLine().split(" ");
       for (int j = 0; j < n; j++) {
-        // 숫자를 우선순위 큐에 넣어준다
-        priorityQueue.offer(Integer.parseInt(lineNums[j]));
-        // 만약 우선순위 큐의 크기가 n보다 커지면,
-        if (priorityQueue.size() > n){
-          // 하나 빼서 n개로 유지한다
-          priorityQueue.poll();
-        }
+        pQueue.offer(Integer.parseInt(lineNums[j]));
       }
     }
-    System.out.println(priorityQueue.peek());
+    // 1~4번째로 큰 숫자 우선 순위 큐에서 빼주기
+    for (int i = 0; i < n-1; i++) {
+      pQueue.poll();
+    }
+    // 5번째로 큰 숫자
+    System.out.println(pQueue.peek());
   }
 }
