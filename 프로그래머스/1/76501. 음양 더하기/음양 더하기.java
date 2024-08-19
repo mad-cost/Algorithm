20d1d8a790638a23a7b72e0f0252ec3170510934
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-08-19 10:45:25 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,11 +1,9 @@
+
 class Solution {
   public int solution(int[] absolutes, boolean[] signs) {
     int sum = 0;
     for (int i = 0; i < absolutes.length; i++) {
-      if (signs[i]==true){
-        sum += absolutes[i];
-      }else 
-        sum -= absolutes[i];
+      sum = (signs[i]==true)?sum+absolutes[i]:sum-absolutes[i];
     }
     return sum;
   }
