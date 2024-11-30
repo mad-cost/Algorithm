6f01b404a8b2a2a785a0dc4866345c64545ce528
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-01 00:00:04 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.45 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,5 +1,14 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(String my_string) {
-    return my_string.trim().split("\\s+");
+        StringTokenizer st = new StringTokenizer(my_string);
+        int N = st.countTokens();
+        String[] result = new String[N];
+        
+        for (int i = 0; i < N; i++){
+            result[i] = st.nextToken();
+        }
+        return result;
     }
 }
