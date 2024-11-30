2f377076a5c841865ac1a2f08240463e3379e801
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-01 05:29:35 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -17,28 +17,28 @@ public class Main {
     }
     Arrays.sort(A);
 
-    for (int k = 0; k < N; k++) { // k = 두 수 i+j의 합의 타켓 값
+    for (int k = 0; k < N; k++) {
       long find = A[k];
       int i = 0; // head_index
       int j = N - 1; // tail_index
 
-      // 투 포인터
       while (i < j) {
-      if (A[i] + A[j] == find){
-        if (i != k && j != k){
-          count++;
-          break;
-        } else if (i == k) {
-          i ++;
-        } else if (j==k) {
+        if (A[i] + A[j] == find) { // i + j = target
+           if (i == k) {
+            i++;
+          } else if (j == k) {
+            j--;
+          } else if(i != j) { // 2 + 2  != 4
+            count++;
+            break;
+          }
+        } else if (A[i] + A[j] < find) { // ex) i+j -> 3 < 5
+          i++;
+        } else {
           j--;
         }
-      }else if (A[i] + A[j] < find){
-        i++;
-      }else {
-        j--;
       }
-      }
+
     }
     System.out.println(count);
   }
