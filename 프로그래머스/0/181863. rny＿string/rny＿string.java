cb5f1050b8e3dbb5bace770dcd6aa52ecd2bbce3
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-11-30 23:22:44 +0900

    [level 0] Title: rny_string, Time: 0.34 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,14 +1,5 @@
 class Solution {
     public String solution(String rny_string) {
-        char[] c = rny_string.toCharArray();
-        String s = "";
-        for (int i = 0; i < c.length; i++){
-            if (c[i] == 'm'){
-                s += "rn";
-            }else{
-                s += c[i];
-            }
-        }
-        return s;
+        return rny_string.replaceAll("m", "rn");
     }
 }
