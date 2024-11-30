cf10c378ca371f17d7792796d7b69c307852d31f
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-01 00:26:43 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.81 ms, Memory: 93 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        StringTokenizer st = new StringTokenizer(my_string);
+        int N = st.countTokens();
+        
+        String[] result = new String[N];
+        for (int i = 0; i < N; i++){
+            result[i] = st.nextToken();    
+        }
+        return result;
+    }
+}
