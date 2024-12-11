ffe0c18090344d225962200c1bea5d9cf457645b
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-11 15:15:15 +0900

    [Silver V] Title: 배열 합치기, Time: 1464 ms, Memory: 317856 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 11728
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());  //첫번째 배열 길이
+    int m = Integer.parseInt(st.nextToken());  //두번째 배열 길이 		
+    int [] arr = new int[n+m];  //정렬할 배열 초기화 
+
+    st = new StringTokenizer(br.readLine());  //첫번째 배열 
+
+    for (int i = 0; i < n; i++) {
+      arr[i]= Integer.parseInt(st.nextToken());
+    }
+    st = new StringTokenizer(br.readLine()); //두번째 배열 
+
+    for (int i = 0; i < m; i++) {
+      arr[i+n] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(arr); //정렬 
+
+    StringBuilder sb = new StringBuilder();
+
+    for(int r : arr) {
+      sb.append(r+" ");
+
+    }
+    System.out.println(sb);
+
+  }
+
+}
+/*
+>>
+2 2
+3 5
+2 9
+<<
+2 3 5 9
+ */
