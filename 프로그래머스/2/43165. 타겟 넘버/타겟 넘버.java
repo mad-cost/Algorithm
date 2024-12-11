06fbb3a3bbc960cfaa75a49596d4ae8181d40f32
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-11 14:01:51 +0900

    [level 2] Title: 타겟 넘버, Time: 242.88 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,28 @@
-class Solution {
-    static int result;
+import java.util.LinkedList;
+import java.util.Queue;
 
-    public int solution(int[] numbers, int target) {
-        DFS(numbers, target,0 ,0);
-        return result;
-    }
-    static void DFS(int[] numbers, int target, int sum, int index){
-        if(index == numbers.length){
-            if(sum == target){
-                result ++;
+class Solution {
+   public int solution(int[] numbers, int target) {
+        int result = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {0, 0}); // sum = 0, index = 0
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int sum = current[0]; 
+            int index = current[1];
+
+    
+            if (index == numbers.length) {
+                if (sum == target) {
+                    result++; 
+                }
             }
-            return;
+            if(index < numbers.length){
+            queue.offer(new int[] {sum + numbers[index], index + 1});
+            queue.offer(new int[] {sum - numbers[index], index + 1});
+            }       
         }
-        DFS(numbers, target, sum + numbers[index], index + 1);
-        DFS(numbers, target, sum - numbers[index], index + 1); 
+        return result;
     }
 }
