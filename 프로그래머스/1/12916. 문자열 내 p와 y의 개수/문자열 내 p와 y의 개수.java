e492ad6459c792e9337dd07fb4434737c1ea34c6
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-12 16:01:08 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.07 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,20 +1,22 @@
 class Solution {
     boolean solution(String s) {
-        //[연습] 포인터 사용해서 풀어보기
+        int[] count = new int[2];
         s = s.toUpperCase();
-        int[] count = {0,0}; // P = 0, Y = 0
-        
-        int startIndex = 0;
-    
-        while(s.length() > startIndex){
-            if(s.charAt(startIndex) == 'P'){
-                count[0] += 1;
-            }else if(s.charAt(startIndex) == 'Y'){
-                count[1] += 1;
-            }
-            startIndex += 1;
+        return DFS(s, 0, count); // DFS 호출 시 count 전달
+    }
+
+    static boolean DFS(String s, int index, int[] count) {
+        // 종료 조건
+        if (index >= s.length()) {
+            return count[0] == count[1] ? true : false;
         }
-        
-        return count[0] == count[1] ? true : false;
+
+        if (s.charAt(index) == 'P') {
+            count[0]++;
+        } else if (s.charAt(index) == 'Y') {
+            count[1]++;
+        }
+        // 다음 인덱스로 재귀 호출
+        return DFS(s, index + 1, count);
     }
 }
