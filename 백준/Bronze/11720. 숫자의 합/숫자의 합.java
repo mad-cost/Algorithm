488645be36ded9b1392ee0e5c2c2348a440b83a2
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-19 14:28:09 +0900

    [Bronze IV] Title: 숫자의 합, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,15 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+// 11720 // 배열 사용하기
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in); // 표준 입력(System.in)을 통해 Scanner 객체 생성
-    int N = sc.nextInt(); // 5
-    String sNum = sc.next(); // 입력 받은 숫자들을 String으로 저장 "54321"
-    char[] cNum = sNum.toCharArray(); // "54321" -> ['5', '4', '3', '2', '1']
-    int sum = 0; // 초기화
-    for (int i = 0; i < cNum.length; i++) { // 0 ~ 4 까지 반복 (5번 반복)
-      sum += cNum[i] - '0'; // TODO i = 0 일 때, cNum[1] - '0' = 53 - 48
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine()); // 5
+    String M = br.readLine(); // 54321
+
+    int[] A = new int[N]; // 0 ~ 4
+    int sum = 0;
+
+    for (int i = 0; i < N; i++) {
+      A[i] = M.charAt(i) - '0'; // '5'(53) - '0'(48) = 5
+      sum += A[i];
     }
-    System.out.print(sum);
+
+    System.out.println(sum);
   }
 }
+
+
+/*
+>>
+5
+54321
+<<
+15
+ */
