146a8360bcd6315b860ca04360e96796b09db98b
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-20 22:51:04 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1328 ms, Memory: 127608 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
+// 11659 누적합 공식 사용 복습
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,8 +11,7 @@ public class Main {
     int N = Integer.parseInt(st.nextToken()); // 4
     int M = Integer.parseInt(st.nextToken()); // 3
 
-    int[][] A = new int[N + 1][N + 1];
-
+    int[][] A = new int[N+1][N+1];
     for (int i = 1; i <= N; i++) {
       st = new StringTokenizer(br.readLine());
       for (int j = 1; j <= N; j++) {
@@ -19,23 +19,39 @@ public class Main {
       }
     }
 
-    int[][] DP = new int[N+1][N+1];
-    // 전체 표의 최종 누적합
-    for (int i = 1; i <= N; i++){
-      for (int j = 1; j <= N; j++){
-        DP[i][j] = DP[i][j-1] + DP[i-1][j] - DP[i-1][j-1] + A[i][j];
-        // DP[i][j] 의 누적 합은 = 내 왼쪽까지의 누적 합 + 내 위쪽까지의 누적 합 - 중복된 누적 합 + 내 위치에 해당하는 자신의 값
+    // 누적 합 생성
+    int[][] S = new int[N+1][N+1];
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= N; j++) {
+        S[i][j] = S[i][j-1] + S[i-1][j] - S[i-1][j-1] + A[i][j]; // 내 왼쪽 + 내 위쪽 - 대각선 + 내자신
       }
     }
 
-    for (int i = 0; i < M; i++){
-      st = new StringTokenizer(br.readLine()); // 2 2 3 4
-      int x1 = Integer.parseInt(st.nextToken()); // 2
-      int y1 = Integer.parseInt(st.nextToken()); // 2
-      int x2 = Integer.parseInt(st.nextToken()); // 3
-      int y2 = Integer.parseInt(st.nextToken()); // 4
-      int result = DP[x2][y2] - DP[x1-1][y2] - DP[x2][y1-1] + DP[x1-1][y1-1];
+    for (int i = 0; i < M; i++) {
+      int result = 0;
+      st = new StringTokenizer(br.readLine());
+      int x1 = Integer.parseInt(st.nextToken());
+      int y1 = Integer.parseInt(st.nextToken());
+      int x2 = Integer.parseInt(st.nextToken());
+      int y2 = Integer.parseInt(st.nextToken());
+
+      result = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1];
       System.out.println(result);
     }
   }
 }
+/*
+>>
+4 3
+1 2 3 4
+2 3 4 5
+3 4 5 6
+4 5 6 7
+2 2 3 4
+3 4 3 4
+1 1 4 4
+<<
+27
+6
+64
+ */
