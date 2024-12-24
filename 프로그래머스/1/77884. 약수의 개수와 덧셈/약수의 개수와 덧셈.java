50dd71a2c3757ee5158fb762fede57862455b67a
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-25 02:31:28 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.61 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,3 +1,4 @@
+// Math.sqrt() 사용하기
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
@@ -5,9 +6,12 @@ class Solution {
         for(int i = left; i <= right; i++){
             int count = 0;
             
-            for(int j = 1; j <= i; j++){
+            for(int j = 1; j <= Math.sqrt(i); j++){
                 if(i % j == 0){
                     count ++;
+                    if(i/j != j){
+                    count ++;
+                    }
                 }
             }
             if(count % 2 == 0){
