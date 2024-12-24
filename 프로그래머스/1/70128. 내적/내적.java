6c054f84050412f9277fb1040de8e596f0a00d04
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-25 01:16:46 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,11 +1,12 @@
+// 계산해주는 메서드 분리하기
 class Solution {
     public int solution(int[] a, int[] b) {
-        int sum = 0;
-        
+        return calculate(a, b, 0);
+    }
+    public static int calculate(int[] a, int[]b, int sum){
         for(int i = 0; i < a.length; i++){
             sum += a[i] * b[i];
         }
-        
         return sum;
     }
 }
