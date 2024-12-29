edfb377e7a20de98b48892737665d5f36b8985f6
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-29 15:04:26 +0900

    [level 1] Title: 행렬의 덧셈, Time: 13.86 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,11 +1,24 @@
-// 가장 쉬운 방법 
+// 재귀호출로 풀어보기
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        for(int i = 0; i<arr1.length; i++){
-            for(int j = 0; j < arr1[0].length; j++){
-                arr1[i][j] += arr2[i][j];
-            }
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        // 재귀 시작
+        return recursive(arr1, arr2, answer, 0, 0);
+    }
+    
+    public int[][] recursive(int[][] arr1, int[][] arr2, int[][] answer, int i, int j) {
+        // 종료 조건
+        if (i == arr1.length) {
+            return answer;
         }
-        return arr1;
+        
+        answer[i][j] = arr1[i][j] + arr2[i][j];
+        
+        // 재귀 조건
+        if (j < arr1[i].length - 1) {
+            return recursive(arr1, arr2, answer, i, j + 1);
+        } else {
+            return recursive(arr1, arr2, answer, i + 1, 0);
+        }
     }
 }
