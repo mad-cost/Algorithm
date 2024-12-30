64087c9472fe292edcdf41552f1cf7faf19af086
서병준 <148847167+mad-cost@users.noreply.github.com>
2024-12-31 01:28:30 +0900

    [level 1] Title: 직사각형 별찍기, Time: 290.79 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
+// 재귀함수를 사용한 해결 방법
 class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         
-        for(int i = 0; i<b; i++){
-            for(int j = 0; j < a; j++){
-                System.out.print("*");
-            }
+        recursive(a, a, b);
+    }
+    // 재귀함수 시작
+    public static void recursive(int originalA, int a, int b) {
+        if (b == 0) {
+            return;
+        }
+        
+        if (a > 0) {
+            System.out.print("*");
+            recursive(originalA, a - 1, b);
+        } else {
             System.out.println();
+            recursive(originalA, originalA, b - 1);
         }
     }
 }
