654368bb542bb90b8b4ed5656b7919eb52673405
서병준 <148847167+mad-cost@users.noreply.github.com>
2025-01-16 17:26:50 +0900

    [Bronze III] Title: 최소, 최대, Time: 440 ms, Memory: 88552 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -3,27 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// Math 클래스 사용해서 풀어보기
 public class Main {
-  static StringTokenizer st ;
-
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine()); // 5
     StringTokenizer st = new StringTokenizer(br.readLine());
 
-    int min = Integer.MAX_VALUE;
     int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
 
     for (int i = 0; i < N; i++) {
       int num = Integer.parseInt(st.nextToken());
-      if (min > num){
-        min = num;
-      }
-      if (max < num){
-        max = num;
-      }
+      max = Math.max(max, num);
+      min = Math.min(min, num);
     }
-
-    System.out.println(min+" "+max);
+    System.out.println(min + " " + max);
   }
 }
