d66766890506dbfc4473b2171af0ba298ae4bd7e
서병준 <148847167+mad-cost@users.noreply.github.com>
2025-01-27 01:58:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1396 ms, Memory: 142312 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,26 +1,57 @@
 import java.io.*;
 import java.util.*;
 
-// HashMap 사용
+// 이분탐색 LowerBound, UpperBound 사용
 class Main {
   static StringTokenizer st;
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    HashMap<Integer, Integer> map = new HashMap<>();
+    StringBuilder sb = new StringBuilder();
+
+    int N  = Integer.parseInt(br.readLine());
+    int[] nArr = new int[N];
 
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
-      int num = Integer.parseInt(st.nextToken());
-      map.put(num, map.getOrDefault(num, 0) + 1);
+      nArr[i] = Integer.parseInt(st.nextToken());
     }
+    Arrays.sort(nArr);
 
     int M = Integer.parseInt(br.readLine());
     st = new StringTokenizer(br.readLine());
+
     for (int i = 0; i < M; i++) {
       int target = Integer.parseInt(st.nextToken());
-      System.out.print(map.getOrDefault(target, 0) + " ");
+      int result = upperBound(nArr,target) - lowerBound(nArr, target);
+      sb.append(result + " ");
     }
+    System.out.print(sb.toString().trim());
+  }
+  private static int lowerBound(int[] nArr, int target){
+    int left = 0;
+    int right = nArr.length;
+    while (left < right){
+      int mid = (left + right) / 2;
+      if (nArr[mid] < target){
+        left = mid + 1;
+      }else {
+        right = mid;
+      }
+    }
+    return  left;
+  }
 
+  private static int upperBound(int[] nArr, int target){
+    int left = 0;
+    int right = nArr.length;
+    while (left < right){
+      int mid = (left + right) / 2;
+      if (nArr[mid] <= target){
+        left = mid + 1;
+      }else {
+        right = mid;
+      }
+    }
+    return  left;
   }
 }
