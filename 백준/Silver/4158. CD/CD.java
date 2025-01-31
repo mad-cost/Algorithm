f74fde26aec915fc0cc88fd254bf81bb8ddce824
서병준 <148847167+mad-cost@users.noreply.github.com>
2025-01-31 15:49:14 +0900

    [Silver V] Title: CD, Time: 992 ms, Memory: 266132 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
-// 4158 HashSet 사용
+// 4158 이분 탐색
 class Main {
   static StringTokenizer st;
 
@@ -16,22 +17,48 @@ class Main {
       int N = Integer.parseInt(st.nextToken());
       int M = Integer.parseInt(st.nextToken());
 
-      int count = 0;
-      HashSet<Integer> set = new HashSet<>();
-
       if (N == 0 && M == 0){
         break;
       }
+
+      int[] nArr = new int[N];
+
       for (int i = 0; i < N; i++) {
-        set.add(Integer.parseInt(br.readLine()));
+        nArr[i] = Integer.parseInt(br.readLine());
       }
+
+      Arrays.sort(nArr); // 이분 탐색은 정렬이 필수
+      int count = 0;
+
       for (int i = 0; i < M; i++) {
-        if (set.contains(Integer.parseInt(br.readLine()))){
+        int target = Integer.parseInt(br.readLine());
+        boolean checked = BinarySearch(nArr, target);
+        if (checked){
           count ++;
         }
       }
       System.out.println(count);
     }
+  }
+
+  public static boolean BinarySearch(int[] Arrays, int target){
+    int startIndex = 0;
+    int endIndex = Arrays.length - 1;
+    boolean result = false;
+
+    while (startIndex <= endIndex){
+      int midIndex = (startIndex + endIndex) / 2;
+      if (target == Arrays[midIndex]){
+        result = true;
+        break;
+      } else if (target < Arrays[midIndex]) {
+        endIndex = midIndex - 1;
+      }else {
+        startIndex = midIndex + 1;
+      }
+    }
+
+    return result;
   }
 }
 /*
