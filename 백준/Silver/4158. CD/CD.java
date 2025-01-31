4227e130f91e23a8258d0291542005f9c3bc14c3
서병준 <148847167+mad-cost@users.noreply.github.com>
2025-01-31 16:22:15 +0900

    [Silver V] Title: CD, Time: 984 ms, Memory: 268740 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -2,63 +2,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
-// 4158 이분 탐색
+// 4158 재귀 함수 사용
 class Main {
   static StringTokenizer st;
+  static int[] arr;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    while (true){
+    while (true) {
       st = new StringTokenizer(br.readLine());
       int N = Integer.parseInt(st.nextToken());
       int M = Integer.parseInt(st.nextToken());
 
-      if (N == 0 && M == 0){
+      if (N == 0 && M == 0) {
         break;
       }
 
-      int[] nArr = new int[N];
-
+      arr = new int[N];
       for (int i = 0; i < N; i++) {
-        nArr[i] = Integer.parseInt(br.readLine());
+        arr[i] = Integer.parseInt(br.readLine());
       }
 
-      Arrays.sort(nArr); // 이분 탐색은 정렬이 필수
+      Arrays.sort(arr);
       int count = 0;
 
       for (int i = 0; i < M; i++) {
         int target = Integer.parseInt(br.readLine());
-        boolean checked = BinarySearch(nArr, target);
-        if (checked){
-          count ++;
+        boolean checked = Recursion(0, arr.length-1, target);
+        if (checked) {
+          count++;
         }
       }
       System.out.println(count);
     }
   }
 
-  public static boolean BinarySearch(int[] Arrays, int target){
-    int startIndex = 0;
-    int endIndex = Arrays.length - 1;
-    boolean result = false;
-
-    while (startIndex <= endIndex){
-      int midIndex = (startIndex + endIndex) / 2;
-      if (target == Arrays[midIndex]){
-        result = true;
-        break;
-      } else if (target < Arrays[midIndex]) {
-        endIndex = midIndex - 1;
-      }else {
-        startIndex = midIndex + 1;
-      }
+  public static boolean Recursion(int startIndex, int endIndex, int findNumber) {
+    // 종료 조건
+    if (startIndex > endIndex) {
+      return false;
     }
 
-    return result;
+    int midIndex = (startIndex + endIndex) / 2;
+
+    if (findNumber < arr[midIndex]) {
+      return Recursion(startIndex, midIndex - 1, findNumber);
+    } else if (arr[midIndex] < findNumber) {
+      return Recursion(midIndex + 1, endIndex, findNumber);
+    } else {
+      return true;
+    }
   }
 }
 /*
