3bae258dc264171fcb5dd90b47def4f615e45c05
서병준 <148847167+mad-cost@users.noreply.github.com>
2025-02-12 19:54:12 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 10.12 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,16 +1,22 @@
+// 재귀함수로 풀어보기
 class Solution {
     public int solution(String t, String p) {
         int pLength = p.length();
         int i = 0;
         int result = 0;
         
-        while(i+pLength <= t.length()){
-            long tSubstring = Long.parseLong(t.substring(i, i+pLength));
-            if(tSubstring <= Long.parseLong(p)){
-                result ++;
-            }
-            i++;
+        return recursion(t, p, pLength, i, result);
+       
+    }
+    
+    private static int recursion(String t, String p, int pLength, int i, int result){
+        if (t.length() < i+pLength){
+            return result;
         }
-        return result;
+        long tLong = Long.parseLong(t.substring(i, i+pLength));
+        if(tLong <= Long.parseLong(p)){
+            result ++;
+        }
+        return recursion(t, p, pLength, i+1, result);
     }
 }
