c70a4fc5ba83a8fd57705c63e28378fbdcae5fca
서병준 <148847167+mad-cost@users.noreply.github.com>
2025-02-13 23:01:13 +0900

    [level 1] Title: 예산, Time: 0.31 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,10 +1,20 @@
 import java.util.*;
-
+// 버블 정렬 적용해보기
 class Solution {
     public int solution(int[] d, int budget) {
         int result = 0;
-        Arrays.sort(d);
+        // Arrays.sort(d);
     
+        for(int i = 0; i < d.length - 1; i++){
+            for(int j = 0; j < d.length - 1 - i; j++){
+                int temp = 0;
+                if(d[j] > d[j+1]){
+                    temp = d[j];
+                    d[j] = d[j+1];
+                    d[j+1] = temp;
+                }
+            }
+        }
         for(int i = 0; i < d.length; i++){
             if(0 <= budget - d[i]){
                 budget -= d[i];
